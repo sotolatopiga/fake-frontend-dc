0c5aae1cf27c7d3c509d2e41ca3edb559a31982b
sotolatopiga <62226731+sotolatopiga@users.noreply.github.com>
2020-11-04 01:16:27 +0700

    Add files via upload

---
+++ minimalBokehServer.py
@@ -0,0 +1,33 @@
+from bokeh.server.server import Server
+from bokeh.util.browser import view
+from bokeh.document.document import Document
+from bokeh.application.handlers import FunctionHandler
+from bokeh.application.application import  Application
+from commonOld import threading_func_wrapper
+from plotBuySell import createPlot, requestData
+from bokeh.layouts import column
+
+
+BOKEH_PORT = 5010
+if "docs" not in globals(): docs = []
+
+def attachDocToServer(doc : Document):
+    p, activate = createPlot()
+    doc.add_root(column(p))
+    docs.append(doc)
+    activate()
+
+
+if __name__ == "__main__":
+    s = Server({'/': Application(FunctionHandler(attachDocToServer))},
+               num_proc=16,
+               port=BOKEH_PORT,
+               allow_websocket_origin=["*"])
+    s.start()
+    threading_func_wrapper(s.io_loop.start, delay=0.01)
+    threading_func_wrapper(lambda: view(f"http://localhost:{BOKEH_PORT}"), 0.5)
+
+# len(docs[-1].get_model_by_name("glyphBuyPressure").data_source.data['index'])
+
+#%%
+sourceBuySell, sourceVolume = requestData()
---
+++ plotBuySell.py
@@ -0,0 +1,78 @@
+import pandas as pd
+from datetime import datetime
+from commonOld import tik, tok
+from bokeh.plotting import Figure, show, output_file
+from bokeh.models import ColumnDataSource, Range1d, CrosshairTool, WheelZoomTool, CustomJS, Div
+from bokeh.layouts import column, row, layout
+from pandas import DataFrame
+from os import listdir
+from os.path import isfile, join
+from flask import jsonify
+import json, pickle
+import time
+import requests
+
+url = 'http://localhost:5000/api/hose-indicators-outbound'
+data = {"volumes": {}, "buySell": {}}
+DEBUG = True
+
+def update(doc):
+    print(f"updated {doc}")
+    source = doc.get_model_by_name("glyphSellPressure").data_source
+    sourceBuySell, sourceVolume = requestData()
+    lsource = len(source.data['index'])
+    lnew = len(sourceBuySell.data['index'])
+    if lnew > lsource:
+        hoseUpdate = {key: sourceBuySell.data[key][lsource: lnew] for key in sourceBuySell.data.keys()}
+        source.stream(hoseUpdate)
+    #hoseUpdate = {key:sourceBuySell.data[key][oldCount: recent] for key in sourceBuySell.data.keys()}
+    # currentSource.stream(hoseUpdate)
+    return
+
+def createPlot():
+    sourceBuySell, sourceVolume = requestData()
+    print(sourceBuySell)
+    p = Figure(plot_width=1200, plot_height=600, name="pltBuySell")
+    p.line(x='index', y='buyPressure', source=sourceBuySell, color='green', name="glyphBuyPressure")
+    p.line(x='index', y='sellPressure', source=sourceBuySell, color='red', name="glyphSellPressure")
+    def activation_function():
+        p._document.add_periodic_callback(lambda: update(p._document), 1000)
+        print("Document activated !")
+    return p, activation_function
+
+
+def requestData():
+    global data
+    res = requests.post(url,json={})
+    data = res.json()
+    dic = {key: data['buySell'][key] for key in ['buyPressure', 'index', 'sellPressure']}
+    sourceBuySell = ColumnDataSource(dic)  # ['buyPressure', 'index', 'sellPressure', 'time']
+    if DEBUG: print(sourceBuySell.data['index'][-1])
+
+    sourceVolume = ColumnDataSource(data['volumes'])  # ['index', 'nnBuy', 'nnSell', 'time', 'totalValue']
+    if DEBUG: print(sourceVolume.data['index'][-1])
+
+    return sourceBuySell, sourceVolume
+
+
+#p, active = createPlot(); show(p)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
