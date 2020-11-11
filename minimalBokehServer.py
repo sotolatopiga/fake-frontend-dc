df28e9d0736c5a1f0326615a99aa68f041e4f13d
sotola <sotolatopiga@gmail.com>
2020-11-11 08:54:03 +0700

    1.07 Adhoc fixdate

--- minimalBokehServer.py
+++ minimalBokehServer.py
@@ -4,7 +4,7 @@ from bokeh.document.document import Document
 from bokeh.application.handlers import FunctionHandler
 from bokeh.application.application import  Application
 from commonOld import threading_func_wrapper
-from plotBuySell import createPlots, requestHoseData, makeMasterPlot
+from plotBuySell import createPlots, requestHoseData, makeMasterPlot, fetchSuuData
 from bokeh.layouts import column
 from bokeh.models import ColumnDataSource
 
